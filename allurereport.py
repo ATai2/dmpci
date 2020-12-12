ab42890bd6c8d30880400d44d86103649baa4619
wangtaihe <wangtaihe@inspur.com>
2020-12-12 11:42:50 +0800

    allurereport.py

---
+++ allurereport.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# author:wangtaihe
+# datetime:2020/12/3 15:05
+# software: PyCharm
+import os, pytest, sys, time
+import Config as config
+from GetSession import DmpLogin
+
+if __name__ == '__main__':
+    arglengh = len(sys.argv)
+    print(arglengh)
+
+    if arglengh == 1:
+        arg1 = None
+        port = None
+    elif arglengh == 2:
+        arg1 = sys.argv[1]
+        port = None
+    elif arglengh == 3:
+        arg1 = sys.argv[1]
+        port = sys.argv[2]
+
+    if port==None:
+        port='8085'
+
+    config.url = "http://localhost:" +port
+    print(config.url)
+    DmpLogin.cookieStr = None
+    pytest.main(["-v", "--html=./report.html"])
--- test_setting.py
+++ test_setting.py
@@ -6,18 +6,25 @@
 
 import util as util
 from GetSession import DmpLogin
+import allure, pytest, os
 
 
 # 系统管理
 
 class TestReport:
     # @pytest.mark.parametrize("case,data,expected", list(list_params), ids=cases)
+    @allure.feature('报错slaveServer信息')
     def test_save_settingdata(self):
         test = DmpLogin()
         post = test.post_api(
             "/slaveServer/saveSlaveServer?name=kettle&hostName=127.0.0.1&port=8080&webAppName=&username=CeNTILgC6rOizgrFBjPCQQ%3D%3D&password=CeNTILgC6rOizgrFBjPCQQ%3D%3D&idSlave=&master=0",
             data=None)
         util.info(post)
-        assert post['rtCode'] == '1'
+        assert (post['rtCode'] == '1' or post['rtCode'] == '1')
 
 
+if __name__ == '__main__':
+    # 执行pytest单元测试，生成 Allure 报告需要的数据存在 /temp 目录
+    pytest.main(["test_setting.py", '--alluredir', './temp'])
+    # 执行命令 allure generate ./temp -o ./report --clean ，生成测试报告
+    os.system('allure generate ./temp -o ./report --clean')
